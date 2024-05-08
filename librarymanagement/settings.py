754d8d6132a864e918d0c072f97ceb60f2c8edaf
Mohd Saqib <125824034+mohdsaqib07@users.noreply.github.com>
2024-05-08 10:18:55 +0530

    Update settings.py

--- librarymanagement/settings.py
+++ librarymanagement/settings.py
@@ -131,5 +131,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mohdsaqib148183@gmail.com'
-EMAIL_HOST_PASSWORD = 'Air@19004'
+EMAIL_HOST_USER = 'your_email'
+EMAIL_HOST_PASSWORD = 'your_password'
